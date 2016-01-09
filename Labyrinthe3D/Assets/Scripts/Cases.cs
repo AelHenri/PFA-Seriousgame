c7145740f169216d496b6bd6690a46d0139c4be7
ffichant <ffichant@enseirb-matmeca.fr>
2016-01-09 19:38:56 +0100

    Labyrinthe pour la présentation
    Création de l'affichage d'un labyrinthe pour la présentation

---
+++ Labyrinthe3D/Assets/Scripts/Cases.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cases : MonoBehaviour {
+    public GameObject[,] cases;
+    public GameObject cell;
+    public int rows;
+    public int columns;
+
+    public void Awake ()
+    {
+
+        cases = new GameObject[columns, rows];
+        for (int i = 0; i < columns; i++)
+        {
+            print(i);    
+            for(int j = 0; j < rows; j++)
+            {
+                cases[i,j] = (GameObject) Instantiate(cell, new Vector3(i-4.5f, 0f, j-4.5f), Quaternion.identity);
+            }
+        }
+    }
+
+
+}
