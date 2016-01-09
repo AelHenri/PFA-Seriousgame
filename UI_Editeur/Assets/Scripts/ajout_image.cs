6fdc7b77f098896f80263b0f7911ba6dc6c3535b
Jälv <thomas.montaigu@laposte.net>
2016-01-09 15:33:18 +0100

    1st commit editeur
    Début de l'éditeur

---
+++ UI_Editeur/Assets/Scripts/ajout_image.cs
@@ -0,0 +1,248 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class ajout_image : MonoBehaviour
+{
+	private bool guiEnable = false;
+
+
+    public string imagePathIndic = null;
+    private bool showAjoutImageIndic = true; 
+    private bool loadImageIndic = false;
+
+    public string imagePathQues = null;
+    private bool showAjoutImageQues = true;
+    private bool loadImageQues = false;
+    
+	FileBrowser fb = new FileBrowser();
+    public Texture2D img_indication = null;
+    public Texture2D img_question = null;
+    WWW www;
+
+    public CanvasGroup canvasGImgButton1; // Set in Unity IDE.
+    public CanvasGroup canvasGimgAnnule; // Set in Unity IDE.
+    public CanvasGroup canvasGImgAjouetImg2;
+    public CanvasGroup canvasGImgAnnule2;
+
+    /* Pour obtenir la taille du Panel et bien positionner l'image */
+    public GameObject PanelExemple; // Set in Unity IDE.
+    float panelHeightExemple;
+    float panelWidthExemple;
+    public GameObject Canvas;
+    float canvasHeight;
+    float canvasWidth;
+
+    /* Paramètres pour le skin de l'explorateur de fichier */
+    public GUISkin[] skins;
+    public Texture2D file, folder, back, drive;
+    public GUIStyle backStyle, cancelStyle, selectStyle; 
+    string[] layoutTypes = { "Type 0", "Type 1" };
+
+
+
+    public void onClick()
+    {
+        guiEnable = true;
+        loadImageIndic = true;
+        //img_indication = new Texture2D(960, 720, TextureFormat.DXT1, false);
+
+    }
+
+    public void onClickAnnuler()
+    {
+        //img_indication = null;
+        showAjoutImageIndic = true;
+        imagePathIndic = null;
+    }
+
+    public void onClickAjoutImageQuestion()
+    {
+        guiEnable = true;
+        loadImageQues = true;
+        //img_question = new Texture2D(960, 720, TextureFormat.DXT1, false);
+    }
+
+    public void onClickAnnuler2()
+    {
+        //img_question = null;
+        showAjoutImageQues = true;
+        imagePathQues = null;
+    }
+
+
+    void Start()
+    {
+        //setup file browser style
+        //  fb.guiSkin = skins[0]; //set the starting skin
+
+        //set the various textures
+        fb.fileTexture = file;
+        fb.directoryTexture = folder;
+        fb.backTexture = back;
+        fb.driveTexture = drive;
+        //show the search bar
+        fb.showSearch = true;
+        //search recursively (setting recursive search may cause a long delay)
+        fb.searchRecursively = true;
+
+        panelHeightExemple = PanelExemple.GetComponent<RectTransform>().rect.height;
+        panelWidthExemple = PanelExemple.GetComponent<RectTransform>().rect.width;
+
+        canvasHeight = Canvas.GetComponent<RectTransform>().rect.height;
+        canvasWidth = Canvas.GetComponent<RectTransform>().rect.width;
+
+        img_indication = new Texture2D(960, 720, TextureFormat.DXT1, false);
+        imagePathIndic = null;
+
+        img_question = new Texture2D(960, 720, TextureFormat.DXT1, false);
+        imagePathQues = null;
+
+    }
+   
+    
+    /*
+     * Fonction qui charge l'image dans la texture "img_indication" pour pouvoir être affichée
+     */
+    IEnumerator LoadATexture(string st, Texture2D img)
+    {
+        www = new WWW(st);
+        yield return www;
+
+        www.LoadImageIntoTexture(img);
+    }
+
+
+
+    /* 
+     * Cette fonction est appelée avant chaque rendu d'image 
+     * C'est ici qu'on ajoute tout les éléments que l'on souhaite afficher a l'écran
+     * l'explorateur de fichier, les images des QCM
+     */   
+    void OnGUI(){
+
+
+        /* Partie qui affiche l'image sélectionnée */
+        if (img_indication != null && imagePathIndic != null)
+        {
+            GUI.DrawTexture(new Rect((panelWidthExemple / 2) - 250, (panelHeightExemple / 2) - 250, 500, 500), img_indication);
+            showAjoutImageIndic = false;
+        }
+
+        if (img_question != null && imagePathQues != null)
+        {
+            GUI.DrawTexture(new Rect(canvasWidth - panelWidthExemple + 100, canvasHeight - panelHeightExemple + 100, 500, 500), img_question);
+            showAjoutImageQues = false;
+        }
+
+
+
+
+        if (guiEnable) {
+            /*
+             * Pour gérer les skins/ modes de vues
+             */
+            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical();
+            GUILayout.Label("Layout Type");
+            fb.setLayout(GUILayout.SelectionGrid(fb.layoutType, layoutTypes, 1));
+            GUILayout.Space(10);
+            //select from available gui skins
+            GUILayout.Label("GUISkin");
+            foreach (GUISkin s in skins)
+            {
+                if (GUILayout.Button(s.name))
+                {
+                    fb.guiSkin = s;
+                }
+            }
+            GUILayout.Space(10);
+            
+            /* 
+             * Partie qui gère l'affichage de l'explorateur de fichier
+             * et la récupération du chemin de l'image à afficher
+             */
+            if (fb.draw ()) {
+                if (fb.outputFile == null)
+                {
+                    Debug.Log("Cancel hit");
+                    guiEnable = false;
+                }
+                else if (loadImageIndic)
+                {
+                    imagePathIndic = fb.outputFile.FullName﻿.ToString();
+                    Debug.Log("imagePathIndic" + " = " + imagePathIndic);
+
+                    StartCoroutine(LoadATexture( ("file:///" + imagePathIndic), img_indication));
+                    guiEnable = false;
+                    loadImageIndic = false;
+                }
+                else if(loadImageQues)
+                {
+                    imagePathQues = fb.outputFile.FullName﻿.ToString();
+                    Debug.Log("imagePathQues" + " = " + imagePathQues);
+
+                    StartCoroutine(LoadATexture( ("file:///" + imagePathQues), img_question));
+                    guiEnable = false;
+                    loadImageQues = false;
+
+                }
+
+					
+				}
+			}
+		
+
+
+    }
+
+
+    /* 
+     * Ici on désactive ou active les boutons d'ajout d'image et d'annulation
+     */
+    void Update()
+    {
+        
+        if (showAjoutImageIndic == false)
+        {
+            canvasGImgButton1.interactable = false;
+            canvasGImgButton1.alpha = 0;
+
+            canvasGimgAnnule.interactable = true;
+            canvasGimgAnnule.alpha = 1;
+        }
+
+        else if (showAjoutImageIndic == true)
+        {
+            canvasGImgButton1.interactable = true;
+            canvasGImgButton1.alpha = 1 ;
+
+            canvasGimgAnnule.interactable = false;
+            canvasGimgAnnule.alpha = 0;
+        }
+
+        if (showAjoutImageQues == false)
+        {
+            canvasGImgAjouetImg2.interactable = false;
+            canvasGImgAjouetImg2.alpha = 0;
+
+            canvasGImgAnnule2.interactable = true;
+            canvasGImgAnnule2.alpha = 1;
+        }
+         
+        else if (showAjoutImageQues == true)
+        {
+            canvasGImgAjouetImg2.interactable = true;
+            canvasGImgAjouetImg2.alpha = 1;
+
+            canvasGImgAnnule2.interactable = false;
+            canvasGImgAnnule2.alpha = 0;
+        }      
+
+
+    }
+
+ 
+}
---
+++ UI_Editeur/Assets/Scripts/validation.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class validation : MonoBehaviour {
+
+    
+    public CanvasGroup canvasGValidation;
+    public CanvasGroup canvasGConfirmation;
+    public ajout_image ajt;
+    
+
+    bool showConfirmation = false;
+
+    string targetPath;
+    string destFile;
+
+    public void onClickValidation()
+    {
+        destFile = System.IO.Path.Combine(targetPath, "image_exemple.jpg");
+        System.IO.File.Copy(ajt.imagePathIndic, destFile, true);
+
+        destFile = System.IO.Path.Combine(targetPath, "image_question.jpg");
+        System.IO.File.Copy(ajt.imagePathIndic, destFile, true);
+
+        showConfirmation = true;
+    }
+
+    public void onClickConfirmation()
+    {
+        showConfirmation = false;
+    }
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        targetPath = Application.dataPath + "/../Fiches";
+
+        if (!System.IO.Directory.Exists(targetPath))
+            System.IO.Directory.CreateDirectory(targetPath);
+      
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ajt.imagePathIndic != null && ajt.imagePathQues!= null)
+        {
+            canvasGValidation.alpha = 1;
+            canvasGValidation.interactable = true;
+        }
+        else
+        {
+            canvasGValidation.alpha = 0;
+            canvasGValidation.interactable = false;
+        }
+
+        if (showConfirmation)
+            canvasGConfirmation.alpha = 1;
+        else
+            canvasGConfirmation.alpha = 0;
+        canvasGConfirmation.interactable = showConfirmation;
+    }
+
+
+}
