00cf5b8647381fdffae66aa4617f81a6bdd06b7c
Hugo Borel <Hugo Borel>
2016-01-21 19:15:17 +0100

    Test pour le jeu de plateau

---
+++ Plateau/Assets/Scripts/Coordinator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coordinator : MonoBehaviour {
+
+    public GameObject Dice;
+    public GameObject Map;
+    public GameObject Player;
+    public int[] playerPos = new int[1];
+
+    private Dice d;
+    private Map m;
+
+	// Use this for initialization
+	void Start () {
+        m = Map.GetComponent<Map>();
+        m.PrepareMap();
+        Vector3 pos = m.tiles[0].transform.position;
+        Player.transform.position = pos;
+        Player.SetActive(true);
+        d = Dice.GetComponent<Dice>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(d.hasBeenRolled)
+        {
+            int temp = playerPos[0];
+            playerPos[0] = (playerPos[0] + d.currentValue) % m.nbTiles;
+            Move move = Player.GetComponent<Move>();
+            move.startPosition.Add(Player.transform.position);
+            for (int i = temp; i != (playerPos[0] + 1) % m.nbTiles; i = (i + 1) % m.nbTiles)
+            {
+                move.startPosition.Add(m.tiles[i].transform.position);
+                move.endPosition.Add(m.tiles[i].transform.position);
+            }    
+            d.hasBeenRolled = false;
+        } 
+	}
+}
---
+++ Plateau/Assets/Scripts/Dice.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dice : MonoBehaviour {
+
+    public int nbFrameToChange = 10;
+    private SpriteRenderer sr;
+    private int nbFrameSinceStart = 0;
+    public Sprite[] sprites = new Sprite[6];
+    private bool roll;
+    public int currentValue;
+    public GameObject indicator;
+    public bool hasBeenRolled = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+	    if(roll)
+        {
+            if (nbFrameSinceStart % nbFrameToChange == 0)
+            {
+                currentValue = Random.Range(1, 7);
+                sr.sprite = sprites[currentValue - 1];
+            }
+            nbFrameSinceStart++;
+        }
+	}
+
+    void OnMouseDown()
+    {
+        roll = !roll;
+        if(!roll)
+            hasBeenRolled = true;
+        indicator.SetActive(!roll);
+    }
+}
