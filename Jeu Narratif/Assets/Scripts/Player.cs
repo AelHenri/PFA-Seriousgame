e8a4502a81a20373bd9460ded04758890d7721ee
Toussaint Henri <t.henri.al@gmail.com>
2016-02-04 22:47:06 +0100

    Jeu narratif début
    Background + perso

---
+++ Jeu Narratif/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public int speed = 1;
+
+    private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+        target = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButton(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+	}
+}
