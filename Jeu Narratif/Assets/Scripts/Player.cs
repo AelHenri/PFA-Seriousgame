d70571c79e8836608e4dc32fc4baf874903b6d87
Toussaint Henri <t.henri.al@gmail.com>
2016-02-07 00:34:53 +0100

    pixie + collisions

--- Jeu Narratif/Assets/Scripts/Player.cs
+++ Jeu Narratif/Assets/Scripts/Player.cs
@@ -6,20 +6,37 @@ public class Player : MonoBehaviour {
     public int speed = 1;
 
     private Vector3 target;
+    private Rigidbody2D rb;
+    private bool collided;
 
 	// Use this for initialization
 	void Start () {
-        target = transform.position;	
+        target = transform.position;
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        
 	    if (Input.GetMouseButton(0))
         {
+            collided = false;
             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             target.z = transform.position.z;
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (!collided)
+        {   
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+        
 	}
+
+    void OnCollisionEnter2D()
+    {
+        collided = true;
+        /*rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0f;*/
+    }
 }
