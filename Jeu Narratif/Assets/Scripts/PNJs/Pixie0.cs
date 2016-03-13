bba3ba0073d16a3065f299f0f6b8b9818df3af37
Toussaint Henri <t.henri.al@gmail.com>
2016-03-13 23:37:06 +0100

    git de merde

--- Jeu Narratif/Assets/Scripts/PNJs/Pixie0.cs
+++ Jeu Narratif/Assets/Scripts/PNJs/Pixie0.cs
@@ -1,24 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pixie0 : PNJ {
+public class Pixie0 : PNJ
+{
 
-<<<<<<< HEAD
     protected override void PNJClickEvent()
     {
         //callPlaceArrows();
-        base.PNJClickEvent(); 
+        base.PNJClickEvent();
     }
 
     protected override void PNJLoadEvent()
     {
         callPlaceArrows();
         base.PNJLoadEvent();
-=======
-    protected override void LaunchPNJEvent()
-    {
-        base.LaunchPNJEvent();
-        callPlaceArrows();        
->>>>>>> origin/master
     }
 }
