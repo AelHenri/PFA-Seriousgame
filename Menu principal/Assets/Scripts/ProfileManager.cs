5302bdd2ca4b038c69ca9ed53b47a66239c89b6b
Jälv <thomas.montaigu@laposte.net>
2016-03-17 21:10:04 +0100

    Début des profils?

---
+++ Menu principal/Assets/Scripts/ProfileManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine.UI;
+using System.IO;
+
+public class ProfileManager : MonoBehaviour {
+
+
+
+
+    public Dropdown profileSelector;
+    //public Text text;
+  
+
+    private string profilesDir;
+    private string[] profilesName;
+    private string[] profilesPath;
+
+    private XDocument xmlProfile;
+
+
+    // Use this for initialization
+    void Start () {
+        profileSelector.ClearOptions();
+
+        profilesDir = Application.dataPath + "/../Profiles";
+        profilesDir = System.IO.Path.GetFullPath(profilesDir);
+
+        /* Gets all the profiles stored in xml files */
+        profilesPath = System.IO.Directory.GetFiles(profilesDir, "*.xml", System.IO.SearchOption.AllDirectories);
+        profilesName = new string[profilesPath.Length];
+
+        /* Puts the student name, written in his xml profile */
+        for (int i = 0; i < profilesPath.Length; i++)
+        {
+            xmlProfile = XDocument.Load(profilesPath[i]);
+            profilesName[i] = xmlProfile.Root.Element("Name").Value;
+        }
+
+        foreach (string s in profilesName)
+        {
+            profileSelector.options.Add(new Dropdown.OptionData() { text = s });     
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //text.text = profilesName[profileSelector.value];
+
+    }
+}
