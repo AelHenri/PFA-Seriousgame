97055e5e82b7bc79c75556cd49c7bf201a2a11cd
Jälv <thomas.montaigu@laposte.net>
2016-03-24 17:38:53 +0100

    Changement réalisation pour les profiles

---
+++ Menu principal/Assets/Scripts/Profile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+
+[Serializable]
+public class Profile {
+
+    string fisrtName;
+    string lastName;
+    bool []isGameFinished;
+
+    List<string> correctlyAnswered;
+    List<bool> collectedObjects;
+
+    public Profile(string firstName, string lastName)
+    {
+        this.fisrtName = firstName;
+        this.lastName = lastName;
+
+        isGameFinished = new bool[3]; // There are only 3 Games
+    }
+
+   public string getFirstName()
+    {
+        return fisrtName;
+    }
+
+    public string getLastName()
+    {
+        return lastName;
+    }
+}
--- Menu principal/Assets/Scripts/ProfileManager.cs
+++ Menu principal/Assets/Scripts/ProfileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Linq;
 using UnityEngine.UI;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class ProfileManager : MonoBehaviour {
 
@@ -10,44 +11,99 @@ public class ProfileManager : MonoBehaviour {
 
 
     public Dropdown profileSelector;
-    //public Text text;
+    public Text text;
   
 
     private string profilesDir;
-    private string[] profilesName;
+    private int profilesCount;
+    
     private string[] profilesPath;
+    private Profile[] profiles;
 
     private XDocument xmlProfile;
 
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         profileSelector.ClearOptions();
 
-        profilesDir = Application.dataPath + "/../Profiles";
-        profilesDir = System.IO.Path.GetFullPath(profilesDir);
-
-        /* Gets all the profiles stored in xml files */
-        profilesPath = System.IO.Directory.GetFiles(profilesDir, "*.xml", System.IO.SearchOption.AllDirectories);
-        profilesName = new string[profilesPath.Length];
-
-        /* Puts the student name, written in his xml profile */
-        for (int i = 0; i < profilesPath.Length; i++)
-        {
-            xmlProfile = XDocument.Load(profilesPath[i]);
-            profilesName[i] = xmlProfile.Root.Element("Name").Value;
-        }
-
-        foreach (string s in profilesName)
-        {
-            profileSelector.options.Add(new Dropdown.OptionData() { text = s });     
-        }
+        openProfilesDir();
         
+        /* Gets all the profiles stored in xml files */
+        profilesPath = Directory.GetFiles(profilesDir, "*.profile", SearchOption.AllDirectories);
+        profilesCount = profilesPath.Length;
+        if (profilesPath == null)
+        {
+            Debug.Log("Pas de profiles dans le dossier");
+            return;
+        }
+        profiles = new Profile[profilesCount];
+        for (int i = 0; i < profilesCount; i++)
+        {
+            profiles[i] = loadExistingProfile(profilesPath[i]);
+        }
+
+        foreach (Profile p in profiles)
+        {
+            profileSelector.options.Add(new Dropdown.OptionData() { text = p.getFirstName() });     
+        }
+
+       //saveNewProfile();
+        //loadExistingProfile();
     }
 	
+    void openProfilesDir()
+    {
+        try
+        {
+            profilesDir = Application.dataPath + "/../Profiles";
+            profilesDir = Path.GetFullPath(profilesDir);
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Debug.Log("Pas de dossier profils");
+        }
+    }
+
+    //TODO better Naming
+    void saveNewProfile()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        Profile pro = new Profile("Vladimir", "Swain");
+        FileStream file = File.Open(Application.dataPath + "/../Profiles/playerInfo.profile", FileMode.Create);
+
+
+        bf.Serialize(file, pro);
+        file.Close();
+
+
+    }
+
+    void saveExistingProfile(Profile profileToBeSaved)
+    {
+
+    }
+    //TODO BETTER NAMING
+    Profile loadExistingProfile(string path)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        //FileStream file = File.Open(Application.dataPath + "/../Profiles/playerInfo.profile", FileMode.Open);
+        FileStream file = File.Open(path, FileMode.Open);
+        
+
+        Profile profile = (Profile)bf.Deserialize(file);
+        /*
+        Debug.Log("Prénom: " + profile.getFirstName());
+        Debug.Log("Nom: " + profile.getLastName());
+        */
+        file.Close();
+        return profile;
+    }
+
 	// Update is called once per frame
 	void Update () {
-        //text.text = profilesName[profileSelector.value];
+
+        text.text = profiles[profileSelector.value].getFirstName();
 
     }
 }
