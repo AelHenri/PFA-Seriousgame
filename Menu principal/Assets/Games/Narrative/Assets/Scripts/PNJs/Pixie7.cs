d4e3623de3f71b57d6594594f84b6d6fc0dd22d9
Toussaint Henri <t.henri.al@gmail.com>
2016-03-30 12:08:56 +0200

    Scène 8 début

---
+++ Menu principal/Assets/Games/Narrative/Assets/Scripts/PNJs/Pixie7.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pixie7 : PNJ{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
